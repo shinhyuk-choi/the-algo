37c07db3b37f8caebc1bf1299ab8fac2379a470e
shchoi <shchoi@payhere.in>
2022-03-02 06:32:12 +0900

    merge_k_sroted_lists

---
+++ leetcode/merge_k_sorted_lists.py
@@ -0,0 +1,29 @@
+import heapq
+from typing import List, Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+def solution(lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+    heap = []
+    root = result = ListNode()
+
+    for i, v in enumerate(lists):
+        heapq.heappush(heap, (v.val, i, v))
+
+    while heap:
+        node = heapq.heappop(heap)
+        idx = node[1]
+        result.next = node[2]
+
+        result = result.next
+        if result.next:
+            heapq.heappush(heap, (result.next.val, idx, result.next))
+
+    return root.enxt
+
+
